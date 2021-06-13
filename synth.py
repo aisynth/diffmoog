df09a302bc1da7353bc15300053599a4ad0b60ed
noy <noyuzi@gmail.com>
2021-06-13 12:04:15 +0300

    Filtering implementation

--- synth.py
+++ synth.py
@@ -9,25 +9,27 @@ import numpy as np
 from numpy import asarray, zeros, place, nan, mod, pi, extract
 import torch.nn as nn
 import torch
-# import torchaudio.functional
+import torchaudio.functional
 import matplotlib.pyplot as plt
 import pytorch_forecasting
+import simpleaudio as sa
 
 PI = 3.141592653589793
 TWO_PI = 2 * PI
+
 
 # data = torchaudio.functional.compute_kaldi_pitch('sine',sample_rate =2200,frame_length=5000)
 
 class Signal:
     def __init__(self):
-        self.freq_sample = 44100
+        self.sample_rate = 44100
         self.sig_duration = 1.0  # in seconds
         self.time_sample = torch.linspace(0, 1, steps=44100)
         self.pi = torch.acos(torch.zeros(1).float()) * 2.0
         self.modulation_time = torch.linspace(0, 1, steps=44100)
         self.modulation = 0
         self.data = 0
-        self.shift = self.sig_duration / (self.freq_sample * 2)
+        self.shift = self.sig_duration / (self.sample_rate * 2)
         self.conv = nn.Conv1d(1, 1, 1, 1)
         torch.wave = 0
 
@@ -69,7 +71,7 @@ class Signal:
             oscillator = amp * 2 * (t * freq - torch.floor(0.5 + t * freq))
             oscillator = torch.roll(oscillator, int(len(t) * phase / (TWO_PI * freq)))
 
-        return oscillator
+        self.data = oscillator
 
     def mix_signal(self, signal1, signal2, factor):
         """Signal superposition. factor balances the mix (0 - signal1 only, 1 - signal2 only, 0.5 evenly balanced)."""
@@ -110,6 +112,7 @@ class Signal:
     (Ac + Am*cos(2*pi*fm*t)) * cos(2*pi*fc*t)
     Ac, amp_mod, fm, fc, must to be float
     '''
+
     def am_modulation(self, fm, fc, Ac, Am):
         modulator = Am * torch.cos(2 * self.pi * fm * self.time_sample)
         carrier = torch.cos(2 * self.pi * fc * self.time_sample)
@@ -120,6 +123,7 @@ class Signal:
     (Ac + input_signal) * cos(2*pi*fc*t)
     Ac, amp_mod, fm, fc, must to be float
     '''
+
     def am_modulation_for_input(self, input_signal, fc, Ac, Am):
         modulator = Am * input_signal
         carrier = torch.cos(2 * self.pi * fc * self.time_sample)
@@ -131,45 +135,48 @@ class Signal:
     A+D+S+R = self.sig_duration ()
     Ys is sustain value, amp is the max point in A
     '''
+
     def adsr_envelope(self, amp, A, D, S, Ys, R):
         time_sample = torch.linspace(0, 1, steps=44000)
         time_sample = torch.where(time_sample <= A, time_sample, 0)
         A_env = time_sample * A / amp
 
-        '''calc D envlope'''
+        '''calc D envleope'''
         time_sample = torch.linspace(0, 1, steps=44000)
         time_sample = torch.where(time_sample > A and time_sample <= A + D, time_sample, 0)
         D_env = (A + D - time_sample) * (A - Ys) / D
 
-        '''calc S envlope'''
+        '''calc S envelope'''
         time_sample = torch.linspace(0, 1, steps=44000)
         time_sample = torch.where(time_sample > A + D and time_sample <= self.sig_duration - R, time_sample, 0)
         S_env = time_sample * Ys
 
-        '''calc R envlope'''
+        '''calc R envelope'''
         time_sample = torch.linspace(0, 1, steps=44000)
         time_sample = torch.where(time_sample > self.sig_duration - R and time_sample <= self.sig_duration,
                                   1 - time_sample, 0)
         R_env = time_sample * Ys / (1 - R)
 
-        '''build envlope'''
+        '''build envelope'''
         envelope = torch.cat([A_env, D_env, S_env, R_env], axis=0)
         self.data = self.data * envelope
 
-    # todo: write functions behavior
-    def low_pass(self, fc, pole):
-        pass
+    def low_pass(self, cutoff_freq, q=0.707):
+        self.data = torchaudio.functional.lowpass_biquad(self.data, self.sample_rate, cutoff_freq, q)
 
-    def band_pass(self, fc, pole):
-        pass
+    def high_pass(self, cutoff_freq, q):
+        self.data = torchaudio.functional.highpass_biquad(self.data, self.sample_rate, cutoff_freq, q)
 
-    def high_pass(self, fc, pole):
-        pass
+    def band_pass(self, central_freq, q=0.707, const_skirt_gain=False):
+        self.data = \
+            torchaudio.functional.bandpass_biquad(self.data, self.sample_rate, central_freq, q, const_skirt_gain)
 
+    # todo: ask Moshe to remove this. It changes only phase of frequencies
     def all_pass(self, fc, pole):
         pass
 
-    def Resonance_filter(self):
+    # todo: ask Moshe to remove this
+    def resonance_filter(self):
         pass
 
     # todo: Apply reverb, echo and filtering using th DDSP library
@@ -180,9 +187,17 @@ class Signal:
 
 
 a = Signal()
-b = a.oscillator(a.time_sample, 0.5, 5, 0, 'triangle')
-plt.plot(b)
-
+a.oscillator(a.time_sample, amp=0.5, freq=100, phase=0, waveform='square')
+print(a.data.shape)
+print(a.data.dtype)
+play_obj = sa.play_buffer(a.data.numpy(), 1, 4, a.sample_rate)
+play_obj.wait_done()
+plt.plot(a.data)
+a.low_pass(1000)
+play_obj = sa.play_buffer(a.data.numpy(), 1, 4, a.sample_rate)
+play_obj.wait_done()
+plt.plot(a.data)
+#
 # def fm_modulation(self, amp_mod, fm, fc, Ac, waveform):
 # a.fm_modulation(1, 3, 5, 1, 'tri')
 # print(a.data)
