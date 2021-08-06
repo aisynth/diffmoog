65c98bd2f9c9a5aa5f8a30c04a612c3d8f177cbb
noy <noyuzi@gmail.com>
2021-08-06 11:10:54 +0300

    add gpu support to ai_synth_dataset

--- ai_synth_dataset.py
+++ ai_synth_dataset.py
@@ -1,3 +1,4 @@
+import torch.cuda
 from torch.utils.data import Dataset
 from config import SAMPLE_RATE, PARAMETERS_FILE, AUDIO_DIR
 import pandas as pd
@@ -7,10 +8,11 @@ import os
 
 class AiSynthDataset(Dataset):
 
-    def __init__(self, parameters_csv, audio_dir, transformation, target_sample_rate):
+    def __init__(self, parameters_csv, audio_dir, transformation, target_sample_rate, device):
         self.parameters = pd.read_csv(parameters_csv)
         self.audio_dir = audio_dir
-        self.transformation = transformation
+        self.device = device
+        self.transformation = transformation.to(device)
         self.target_sample_rate = target_sample_rate
 
     def __len__(self):
@@ -20,6 +22,7 @@ class AiSynthDataset(Dataset):
         audio_path = self._get_audio_path(index)
         sound_parameters = self._get_audio_parameters(index)
         sound_signal, sr = torchaudio.load(audio_path)
+        sound_signal = sound_signal.to(self.device)
         transformed_sound_signal = self.transformation(sound_signal)
         return transformed_sound_signal, sound_parameters
 
@@ -35,6 +38,12 @@ class AiSynthDataset(Dataset):
 
 if __name__ == "__main__":
 
+    if torch.cuda.is_available()():
+        device = "cuda"
+    else:
+        device = "cpu"
+    print(f"Using device {device}")
+
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
         sample_rate=SAMPLE_RATE,
         n_fft=1024,
@@ -42,6 +51,6 @@ if __name__ == "__main__":
         n_mels=64
     )
 
-    ai_synth_dataset = AiSynthDataset(PARAMETERS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE)
+    ai_synth_dataset = AiSynthDataset(PARAMETERS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE, device)
     print(f"there are {len(ai_synth_dataset)} files in the dataset")
     signal, parameters = ai_synth_dataset[0]
