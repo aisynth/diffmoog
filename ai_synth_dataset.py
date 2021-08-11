bf3efb592a18720b3f5eb7d77c59c7959384a7bb
noy <noyuzi@gmail.com>
2021-08-11 11:51:50 +0300

    add whole system - working. need refactoring. model predicted params has no limits

--- ai_synth_dataset.py
+++ ai_synth_dataset.py
@@ -1,6 +1,7 @@
 import torch.cuda
 from torch.utils.data import Dataset
-from config import SAMPLE_RATE, PARAMETERS_FILE, AUDIO_DIR, WAVE_TYPE_DIC, FILTER_TYPE_DIC, OSC_FREQ_DIC
+from config import SAMPLE_RATE, PARAMETERS_FILE, AUDIO_DIR, WAVE_TYPE_DIC, FILTER_TYPE_DIC, OSC_FREQ_DIC, \
+    CLASSIFICATION_PARAMETERS_LIST, REGRESSION_PARAMETERS_LIST
 import pandas as pd
 import torchaudio
 import os
@@ -20,11 +21,11 @@ class AiSynthDataset(Dataset):
 
     def __getitem__(self, index):
         audio_path = self._get_audio_path(index)
-        sound_parameters = self._get_audio_parameters(index)
+        sound_classification_parameters, sound_regression_parameters = self._get_audio_parameters(index)
         sound_signal, sr = torchaudio.load(audio_path)
         sound_signal = sound_signal.to(self.device)
         transformed_sound_signal = self.transformation(sound_signal)
-        return transformed_sound_signal, sound_parameters
+        return transformed_sound_signal, sound_classification_parameters, sound_regression_parameters
 
     def _get_audio_path(self, index):
         audio_file_name = f"sound_{index}.wav"
@@ -40,23 +41,26 @@ class AiSynthDataset(Dataset):
         :return: parameters dictionary, containing values for each parameter
         """
         parameters_pd_series = self.parameters.iloc[index]
-        parameters_pd_series.drop(labels=["Unnamed: 0", "file_name"], inplace=True)
-        parameter_dic = parameters_pd_series.to_dict()
+        parameters_pd_series = parameters_pd_series.drop(labels=["Unnamed: 0", "file_name"])
+        regression_parameters_pd_series = parameters_pd_series.loc[REGRESSION_PARAMETERS_LIST]
+        regression_parameters_pd_series = regression_parameters_pd_series.to_dict()
+        classification_parameters_pd_series = parameters_pd_series.loc[CLASSIFICATION_PARAMETERS_LIST]
+        classification_parameters_pd_series = classification_parameters_pd_series.to_dict()
         # map string attributes to numbers
-        for key, val in parameter_dic.items():
+        for key, val in classification_parameters_pd_series.items():
             if "osc1_freq" == key or "osc2_freq" == key:
-                parameter_dic[key] = OSC_FREQ_DIC[round(val, 4)]
+                classification_parameters_pd_series[key] = OSC_FREQ_DIC[round(val, 4)]
             if isinstance(val, str):
                 if "wave" in key:
-                    parameter_dic[key] = WAVE_TYPE_DIC[val]
+                    classification_parameters_pd_series[key] = WAVE_TYPE_DIC[val]
                 elif "filter_type" == key:
-                    parameter_dic[key] = FILTER_TYPE_DIC[val]
-        return parameter_dic
+                    classification_parameters_pd_series[key] = FILTER_TYPE_DIC[val]
+        return classification_parameters_pd_series, regression_parameters_pd_series
 
 
 if __name__ == "__main__":
 
-    if torch.cuda.is_available()():
+    if torch.cuda.is_available():
         device = "cuda"
     else:
         device = "cpu"
--- train.py
+++ train.py
@@ -2,10 +2,11 @@ import torch
 import torchaudio
 from torch import nn
 from torch.utils.data import DataLoader
-from config import BATCH_SIZE, EPOCHS, LEARNING_RATE
+from config import BATCH_SIZE, EPOCHS, LEARNING_RATE, DEBUG_MODE
 from ai_synth_dataset import AiSynthDataset
 from config import SAMPLE_RATE, PARAMETERS_FILE, AUDIO_DIR
 from synth_model import SynthNetwork
+from sound_generator import SynthBasicFlow
 
 
 def create_data_loader(train_data, batch_size):
@@ -13,19 +14,65 @@ def create_data_loader(train_data, batch_size):
     return train_dataloader
 
 
-def train_single_epoch(model, data_loader, loss_fn, optimiser, device):
-    for audio_mel_spectrogram, target_parameters in data_loader:
+def train_single_epoch(model, data_loader, optimiser, device):
+    for audio_mel_spectrogram, classification_target_params, regression_target_parameters in data_loader:
+
         audio_mel_spectrogram = move_to(audio_mel_spectrogram, device)
-        print(target_parameters)
-        target_parameters = move_to(target_parameters, device)
-        # input, target_parameters = input.to(device), target_parameters.to(device)
+        classification_target_params = move_to(classification_target_params, device)
+        regression_target_parameters = move_to(regression_target_parameters, device)
 
         # calculate loss
-        predicted_parameters = model(audio_mel_spectrogram)
-        print(predicted_parameters)
-        print("----------------------------------------------------------------------------------------")
-        print(target_parameters)
+        # todo: force the model to predict values from defined ranges
+        predicted_params = model(audio_mel_spectrogram)
+        # todo: refactor code. extract predicted params
+        predicted_dic = {
+            'osc1_freq': torch.argmax(predicted_params['osc1_freq'], dim=1),
+            'osc1_wave': torch.argmax(predicted_params['osc1_wave'], dim=1),
+            'lfo1_wave': torch.argmax(predicted_params['lfo1_wave'], dim=1),
+            'osc2_freq': torch.argmax(predicted_params['osc1_freq'], dim=1),
+            'osc2_wave': torch.argmax(predicted_params['osc1_wave'], dim=1),
+            'lfo2_wave': torch.argmax(predicted_params['lfo1_wave'], dim=1),
+            'filter_type': torch.argmax(predicted_params['filter_type'], dim=1),
+            'osc1_amp': predicted_params['regression_params'][:, 0],
+            'osc1_mod_index': predicted_params['regression_params'][:, 1],
+            'lfo1_freq': predicted_params['regression_params'][:, 2],
+            'lfo1_phase': predicted_params['regression_params'][:, 3],
+            'osc2_amp': predicted_params['regression_params'][:, 4],
+            'osc2_mod_index': predicted_params['regression_params'][:, 5],
+            'lfo2_freq': predicted_params['regression_params'][:, 6],
+            'lfo2_phase': predicted_params['regression_params'][:, 7],
+            'filter_freq': predicted_params['regression_params'][:, 8],
+            'attack_t': predicted_params['regression_params'][:, 9],
+            'decay_t': predicted_params['regression_params'][:, 10],
+            'sustain_t': predicted_params['regression_params'][:, 11],
+            'release_t': predicted_params['regression_params'][:, 12],
+            'sustain_level': predicted_params['regression_params'][:, 13]
+        }
+        criterion_spectrogram = nn.MSELoss()
+        loss_spectrogram = 0
+        current_predicted_dic = {}
+        # todo: refactor code. try to implement SynthBasicFlow in matrix, to prevent for loop. redefine range
+        #  with general batch size. compute all spectrogram loss all at once using mean function
+        for i in range(predicted_params['osc1_freq'].shape[0]):
+            for key, value in predicted_dic.items():
+                current_predicted_dic[key] = predicted_dic[key][i]
+            synth_obj = SynthBasicFlow(current_predicted_dic)
+            # todo: refactor code. define this transformation in config or other general file
+            mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+                sample_rate=SAMPLE_RATE,
+                n_fft=1024,
+                hop_length=512,
+                n_mels=64
+            )
+            predicted_mel_spec_sound_signal = mel_spectrogram(synth_obj.audio)
+            predicted_mel_spec_sound_signal = move_to(predicted_mel_spec_sound_signal, device)
+            # todo: refactor code. use unsqueeze instead of '0'.
+            current_loss_spectrogram = criterion_spectrogram(predicted_mel_spec_sound_signal, audio_mel_spectrogram[i][0])
+            loss_spectrogram = loss_spectrogram + current_loss_spectrogram
 
+        loss_spectrogram = loss_spectrogram / predicted_params['osc1_freq'].shape[0]
+
+        # todo: use for loop, prevent code duplication
         loss_osc1_freq = nn.CrossEntropyLoss()
         loss_osc1_wave = nn.CrossEntropyLoss()
         loss_lfo1_wave = nn.CrossEntropyLoss()
@@ -35,17 +82,34 @@ def train_single_epoch(model, data_loader, loss_fn, optimiser, device):
         loss_filter_type = nn.CrossEntropyLoss()
         loss_regression_params = nn.MSELoss()
 
-        loss_osc1_freq = loss_osc1_freq(predicted_parameters['osc1_freq'], target_parameters['osc1_freq'])
-        loss_osc1_wave = loss_osc1_wave(predicted_parameters['osc1_wave'], target_parameters['osc1_wave'])
-        loss_lfo1_wave = loss_lfo1_wave(predicted_parameters['lfo1_wave'], target_parameters['lfo1_wave'])
-        loss_osc2_freq = loss_osc2_freq(predicted_parameters['osc2_freq'], target_parameters['osc2_freq'])
-        loss_osc2_wave = loss_osc2_wave(predicted_parameters['osc2_wave'], target_parameters['osc2_wave'])
-        loss_lfo2_wave = loss_lfo2_wave(predicted_parameters['lfo2_wave'], target_parameters['lfo2_wave'])
-        loss_filter_type = loss_filter_type(predicted_parameters['filter_type'], target_parameters['filter_type'])
+        loss_osc1_freq = loss_osc1_freq(predicted_params['osc1_freq'], classification_target_params['osc1_freq'])
+        loss_osc1_wave = loss_osc1_wave(predicted_params['osc1_wave'], classification_target_params['osc1_wave'])
+        loss_lfo1_wave = loss_lfo1_wave(predicted_params['lfo1_wave'], classification_target_params['lfo1_wave'])
+        loss_osc2_freq = loss_osc2_freq(predicted_params['osc2_freq'], classification_target_params['osc2_freq'])
+        loss_osc2_wave = loss_osc2_wave(predicted_params['osc2_wave'], classification_target_params['osc2_wave'])
+        loss_lfo2_wave = loss_lfo2_wave(predicted_params['lfo2_wave'], classification_target_params['lfo2_wave'])
+        loss_filter_type = \
+            loss_filter_type(predicted_params['filter_type'], classification_target_params['filter_type'])
 
+        # todo: refactor code. the code gets dictionary of tensors (regression_target_parameters) and return 2d tensor
+        regression_target_parameters_tensor = torch.empty((len(regression_target_parameters['osc1_amp']), 1))
+        regression_target_parameters_tensor = move_to(regression_target_parameters_tensor, device)
+        for key, value in regression_target_parameters.items():
+            regression_target_parameters_tensor = \
+                torch.cat([regression_target_parameters_tensor, regression_target_parameters[key].unsqueeze(dim=1)],
+                          dim=1)
+        regression_target_parameters_tensor = regression_target_parameters_tensor[:, 1:]
+        regression_target_parameters_tensor = regression_target_parameters_tensor.float()
 
-        loss_regression_params = loss_regression_params(predicted_parameters['regression_params'],)
-        # loss = loss_fn(predicted_parameters, target_parameters)
+        loss_classification_params = \
+            loss_osc1_freq + loss_osc1_wave + loss_lfo1_wave + \
+            loss_osc2_freq + loss_osc2_wave + loss_lfo2_wave + \
+            loss_filter_type
+
+        loss_regression_params = \
+            loss_regression_params(predicted_params['regression_params'], regression_target_parameters_tensor)
+
+        loss = loss_classification_params + loss_regression_params + loss_spectrogram
 
         # backpropogate error and update wights
         optimiser.zero_grad()
@@ -53,12 +117,29 @@ def train_single_epoch(model, data_loader, loss_fn, optimiser, device):
         optimiser.step()
 
     print(f"loss: {loss.item()}")
+    if DEBUG_MODE:
+        print("osc1_freq",
+              torch.argmax(predicted_params['osc1_freq'], dim=1), classification_target_params['osc1_freq'])
+        print("osc1_wave",
+              torch.argmax(predicted_params['osc1_wave'], dim=1), classification_target_params['osc1_wave'])
+        print("lfo1_wave",
+              torch.argmax(predicted_params['lfo1_wave'], dim=1), classification_target_params['lfo1_wave'])
+        print("osc2_freq",
+              torch.argmax(predicted_params['osc2_freq'], dim=1), classification_target_params['osc2_freq'])
+        print("osc2_wave",
+              torch.argmax(predicted_params['osc2_wave'], dim=1), classification_target_params['osc2_wave'])
+        print("lfo2_wave",
+              torch.argmax(predicted_params['lfo2_wave'], dim=1), classification_target_params['lfo2_wave'])
+        print("filter_type",
+              torch.argmax(predicted_params['filter_type'], dim=1), classification_target_params['filter_type'])
+        print("regression_params",
+              predicted_params['regression_params'], regression_target_parameters_tensor)
 
 
-def train(model, data_loader, loss_fn, optimiser, device, epochs):
+def train(model, data_loader, optimiser, device, epochs):
     for i in range(epochs):
         print(f"Epoch {i + 1}")
-        train_single_epoch(model, data_loader, loss_fn, optimiser, device)
+        train_single_epoch(model, data_loader, optimiser, device)
         print("--------------------------------------")
     print("Finished training")
 
@@ -78,7 +159,6 @@ def move_to(obj, device):
         return res
     else:
         raise TypeError("Invalid type for move_to")
-
 
 
 if __name__ == "__main__":
@@ -103,14 +183,11 @@ if __name__ == "__main__":
     # construct model and assign it to device
     synth_net = SynthNetwork().to(device)
 
-    # initialize loss function + optimizer
-    # todo: delete loss pass as argument
-    # loss_fn = nn.CrossEntropyLoss()
-    loss_fn = None
+    # initialize optimizer
     optimiser = torch.optim.Adam(synth_net.parameters(), lr=LEARNING_RATE)
 
     # train model
-    train(synth_net, train_dataloader, loss_fn, optimiser, device, EPOCHS)
+    train(synth_net, train_dataloader, optimiser, device, EPOCHS)
 
     # save model
     torch.save(synth_net.state_dict(), "synth_net.pth")
