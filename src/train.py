33b4497187e47741c04864073a49e72d31ce527a
noy <noyuzi@gmail.com>
2021-09-17 13:24:40 +0300

    LOSS_MODE (SPECTROGAM_ONLY or FULL) is now configurable

--- src/train.py
+++ src/train.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from src.config import BATCH_SIZE, EPOCHS, LEARNING_RATE, DEBUG_MODE, PRINT_TRAIN_STATS, DATASET_MODE
+from src.config import BATCH_SIZE, EPOCHS, LEARNING_RATE, DEBUG_MODE, REGRESSION_LOSS_FACTOR,\
+    SPECTROGRAM_LOSS_FACTOR, PRINT_TRAIN_STATS, DATASET_MODE, LOSS_MODE
 from src.ai_synth_dataset import AiSynthDataset
 from src.config import PARAMETERS_FILE
 from synth_model import SynthNetwork
@@ -17,22 +18,14 @@ def create_data_loader(train_data, batch_size):
 
 
 def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
+    normalizer = helper.Normalizer()
     for signal_log_mel_spec, target_params_dic in data_loader:
         start = time.time()
-
-        classification_target_params = target_params_dic['classification_params']
-        regression_target_parameters = target_params_dic['regression_params']
-        helper.map_classification_params_to_ints(classification_target_params)
-        normalizer = helper.Normalizer()
-        normalizer.normalize(regression_target_parameters)
-
-        classification_target_params = helper.move_to(classification_target_params, device_arg)
-        regression_target_parameters = helper.move_to(regression_target_parameters, device_arg)
-        signal_log_mel_spec = helper.move_to(signal_log_mel_spec, device_arg)
 
         if DEBUG_MODE:
             helper.plot_spectrogram(signal_log_mel_spec[0][0].cpu(), title="MelSpectrogram (dB)", ylabel='mel freq')
 
+        # Run model
         output_dic = model(signal_log_mel_spec)
 
         # Infer predictions
@@ -61,6 +54,7 @@ def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
                 else:
                     current_predicted_dic[key] = predicted_dic[key][i]
 
+            # Generate sound from predicted parameters
             synth_obj = SynthBasicFlow(parameters_dict=current_predicted_dic)
 
             predicted_log_mel_spec_sound_signal = helper.log_mel_spec_transform(synth_obj.signal)
@@ -74,18 +68,56 @@ def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
 
         loss_spectrogram_total = loss_spectrogram_total / len(signal_log_mel_spec)
 
-        # todo: refactor code. the code gets dictionary of tensors (regression_target_parameters) and return 2d tensor
-        regression_target_parameters_tensor = torch.empty((len(regression_target_parameters['osc1_amp']), 1))
-        regression_target_parameters_tensor = helper.move_to(regression_target_parameters_tensor, device_arg)
-        for key, value in regression_target_parameters.items():
-            regression_target_parameters_tensor = \
-                torch.cat([regression_target_parameters_tensor, regression_target_parameters[key].unsqueeze(dim=1)],
-                          dim=1)
-        regression_target_parameters_tensor = regression_target_parameters_tensor[:, 1:]
-        regression_target_parameters_tensor = regression_target_parameters_tensor.float()
+        if LOSS_MODE == "FULL":
+            classification_target_params = target_params_dic['classification_params']
+            regression_target_parameters = target_params_dic['regression_params']
+            helper.map_classification_params_to_ints(classification_target_params)
+            normalizer.normalize(regression_target_parameters)
 
-        loss = loss_spectrogram_total
-        loss.requires_grad = True
+            classification_target_params = helper.move_to(classification_target_params, device_arg)
+            regression_target_parameters = helper.move_to(regression_target_parameters, device_arg)
+
+            criterion_osc1_freq = criterion_osc1_wave = criterion_lfo1_wave \
+                = criterion_osc2_freq = criterion_osc2_wave = criterion_lfo2_wave \
+                = criterion_filter_type \
+                = nn.CrossEntropyLoss()
+            criterion_regression_params = nn.MSELoss()
+
+            loss_osc1_freq = criterion_osc1_freq(output_dic['osc1_freq'], classification_target_params['osc1_freq'])
+            loss_osc1_wave = criterion_osc1_wave(output_dic['osc1_wave'], classification_target_params['osc1_wave'])
+            loss_lfo1_wave = criterion_lfo1_wave(output_dic['lfo1_wave'], classification_target_params['lfo1_wave'])
+            loss_osc2_freq = criterion_osc2_freq(output_dic['osc2_freq'], classification_target_params['osc2_freq'])
+            loss_osc2_wave = criterion_osc2_wave(output_dic['osc2_wave'], classification_target_params['osc2_wave'])
+            loss_lfo2_wave = criterion_lfo2_wave(output_dic['lfo2_wave'], classification_target_params['lfo2_wave'])
+            loss_filter_type = \
+                criterion_filter_type(output_dic['filter_type'], classification_target_params['filter_type'])
+
+            # todo: refactor code. the code gets dictionary of tensors (regression_target_parameters) and return 2d tensor
+            regression_target_parameters_tensor = torch.empty((len(regression_target_parameters['osc1_amp']), 1))
+            regression_target_parameters_tensor = helper.move_to(regression_target_parameters_tensor, device_arg)
+            for key, value in regression_target_parameters.items():
+                regression_target_parameters_tensor = \
+                    torch.cat([regression_target_parameters_tensor, regression_target_parameters[key].unsqueeze(dim=1)],
+                              dim=1)
+            regression_target_parameters_tensor = regression_target_parameters_tensor[:, 1:]
+            regression_target_parameters_tensor = regression_target_parameters_tensor.float()
+
+            loss_classification_params = \
+                loss_osc1_freq + loss_osc1_wave + loss_lfo1_wave + \
+                loss_osc2_freq + loss_osc2_wave + loss_lfo2_wave + \
+                loss_filter_type
+
+            loss_regression_params = \
+                criterion_regression_params(output_dic['regression_params'], regression_target_parameters_tensor)
+
+            loss = \
+                loss_classification_params \
+                + REGRESSION_LOSS_FACTOR * loss_regression_params\
+                + SPECTROGRAM_LOSS_FACTOR * loss_spectrogram_total
+
+        if LOSS_MODE == 'SPECTROGRAM_ONLY':
+            loss = loss_spectrogram_total
+            loss.requires_grad = True
 
         # backpropogate error and update wights
         optimizer_arg.zero_grad()
@@ -93,8 +125,19 @@ def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
         optimizer_arg.step()
 
         if PRINT_TRAIN_STATS:
-            print('loss_spectrogram_total', loss_spectrogram_total)
-            print('\n')
+            if LOSS_MODE == 'FULL':
+                print("-----Classification params-----")
+                print('loss_osc1_freq', loss_osc1_freq)
+                print('loss_osc1_wave', loss_osc1_wave)
+                print('loss_lfo1_wave', loss_lfo1_wave)
+                print('loss_osc2_freq', loss_osc2_freq)
+                print('loss_osc2_wave', loss_osc2_wave)
+                print('loss_lfo2_wave', loss_lfo2_wave)
+                print('loss_filter_type', loss_filter_type)
+                print("-----Regression params-----")
+                print('loss_regression_params', REGRESSION_LOSS_FACTOR * loss_regression_params)
+                print('loss_spectrogram_total', SPECTROGRAM_LOSS_FACTOR * loss_spectrogram_total)
+                print('\n')
             print("-----Total Loss-----")
             print(f"loss: {loss.item()}")
             print("--------------------")
@@ -125,7 +168,7 @@ def train(model, data_loader, optimiser_arg, device_arg, epochs):
     for i in range(epochs):
         print(f"Epoch {i + 1}")
         train_single_epoch(model, data_loader, optimiser_arg, device_arg)
-        print("--------------------------------------")
+        print("--------------------------------------\n")
     print("Finished training")
 
 
@@ -146,6 +189,7 @@ if __name__ == "__main__":
     optimiser = torch.optim.Adam(synth_net.parameters(), lr=LEARNING_RATE)
 
     # train model
+    print("Training model with LOSS_MODE: ", LOSS_MODE)
     train(synth_net, train_dataloader, optimiser, device, EPOCHS)
 
     # save model
