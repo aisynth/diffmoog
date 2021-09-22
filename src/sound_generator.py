gradient backprop fixing. bypassing filter and adsr at synth. adding debugging to watch for nan values

src/sound_generator.py
@@ -6,6 +6,7 @@ import random
 import simpleaudio as sa
 import numpy as np
 import torch
+import helper
 
 
 class SynthBasicFlow:
@@ -147,22 +148,36 @@ class SynthBasicFlow:
         audio.signal = (oscillator1.signal + oscillator2.signal) / 2
         audio.signal = audio.signal.cpu()
 
-        for i in range(num_sounds):
-            if num_sounds == 1:
-                filter_frequency = filter_freq
-            elif num_sounds > 1:
-                if torch.is_tensor(filter_freq[i]):
-                    filter_frequency = filter_freq[i].item()
-                else:
-                    filter_frequency = filter_freq[i]
-            if filter_type[i] == 'high_pass':
-                audio.high_pass(cutoff_freq=filter_frequency, index=i)
-            elif filter_type[i] == 'low_pass':
-                audio.low_pass(cutoff_freq=filter_frequency, index=i)
-            elif filter_type[i] == "band_pass":
-                audio.band_pass(central_freq=filter_frequency, index=i)
+        # for i in range(num_sounds):
+        #     if num_sounds == 1:
+        #         filter_frequency = filter_freq
+        #     elif num_sounds > 1:
+        #         filter_frequency = filter_freq[i]
+        #
+        #     high_pass_signal = audio.high_pass(cutoff_freq=filter_frequency, index=i)
+        #     low_pass_signal = audio.low_pass(cutoff_freq=filter_frequency, index=i)
+        #     band_pass_signal = audio.band_pass(central_freq=filter_frequency, index=i)
+        #
+        #     if isinstance(filter_type, str):
+        #         if filter_type == 'high_pass':
+        #             audio.signal[i] = high_pass_signal
+        #         elif filter_type == 'low_pass':
+        #             audio.signal[i] = low_pass_signal
+        #         elif filter_type == "band_pass":
+        #             audio.signal[i] = band_pass_signal
+        #     else:
+        #         if num_sounds == 1:
+        #             filter_type_probabilities = filter_type
+        #         else:
+        #             filter_type_probabilities = filter_type[i]
+        #
+        #         filter_type_probabilities = filter_type_probabilities.cpu()
+        #         audio.signal[i] = filter_type_probabilities[0] * high_pass_signal \
+        #                           + filter_type_probabilities[1] * low_pass_signal \
+        #                           + filter_type_probabilities[2] * band_pass_signal
+        #         audio.signal[i] = audio.signal[i].to(helper.get_device())
 
-        audio.adsr_envelope(attack_t, decay_t, sustain_t, sustain_level, release_t, num_sounds)
+        # audio.adsr_envelope(attack_t, decay_t, sustain_t, sustain_level, release_t, num_sounds)
 
         return audio.signal
 

src/synth.py
@@ -51,10 +51,10 @@ class Signal:
     def __init__(self, num_sounds=1):
         self.sample_rate = SAMPLE_RATE
         self.sig_duration = SIGNAL_DURATION_SEC
-        self.time_samples = torch.linspace(0, self.sig_duration, steps=int(self.sample_rate * self.sig_duration))
+        self.time_samples = torch.linspace(0, self.sig_duration, steps=int(self.sample_rate * self.sig_duration), requires_grad=True)
         self.modulation_time = torch.linspace(0, self.sig_duration, steps=self.sample_rate)
         self.modulation = 0
-        self.signal = torch.zeros(size=(num_sounds, self.time_samples.shape[0]), dtype=torch.float32)
+        self.signal = torch.zeros(size=(num_sounds, self.time_samples.shape[0]), dtype=torch.float32, requires_grad=True)
 
         self.device = helper.get_device()
         self.time_samples = helper.move_to(self.time_samples, self.device)
@@ -72,7 +72,7 @@ class Signal:
                 amp: Amplitude in range [0, 1]
                 freq: Frequency in range [0, 22000]
                 phase: Phase in range [0, 2pi], default is 0
-                waveform: a string, one of ['sine', 'square', 'triangle', 'sawtooth'] or a list of strings
+                waveform: a string, one of ['sine', 'square', 'triangle', 'sawtooth'] or a probability vector
                 num_sounds: number of sounds to process
 
             Returns:
@@ -84,30 +84,38 @@ class Signal:
 
         self.signal_values_sanity_check(amp, freq, waveform)
         t = self.time_samples
-        oscillator = torch.zeros_like(t)
+        oscillator = torch.zeros_like(t, requires_grad=True)
         for i in range(num_sounds):
-            if not isinstance(waveform, str):
-                waveform_str = waveform[i]
-            else:
-                waveform_str = waveform
-            if num_sounds > 1:
-                freq_float = freq[i]
-            else:
+
+            if num_sounds == 1:
                 freq_float = freq
-            # phase_float = phase[i]
-            if waveform_str == 'sine':
-                oscillator = amp * torch.sin(TWO_PI * freq_float * t + phase)
-            elif waveform_str == 'square':
-                oscillator = amp * torch.sign(torch.sin(TWO_PI * freq_float * t + phase))
-            elif waveform_str == 'triangle':
-                oscillator = (2 * amp / PI) * torch.arcsin(torch.sin((TWO_PI * freq_float * t + phase)))
-            elif waveform_str == 'sawtooth':
-                # Sawtooth closed form
-                oscillator = 2 * (t * freq_float - torch.floor(0.5 + t * freq_float))
-                # Phase shift (by normalization to range [0,1] and modulo operation)
-                oscillator = (((oscillator + 1) / 2) + phase / TWO_PI) % 1
-                # re-normalization to range [-amp, amp]
-                oscillator = amp * (oscillator * 2 - 1)
+            else:
+                freq_float = freq[i]
+
+            sine_wave = amp * torch.sin(TWO_PI * freq_float * t + phase)
+            square_wave = amp * torch.sign(torch.sin(TWO_PI * freq_float * t + phase))
+            triangle_wave = (2 * amp / PI) * torch.arcsin(torch.sin((TWO_PI * freq_float * t + phase)))
+            sawtooth_wave = 2 * (t * freq_float - torch.floor(0.5 + t * freq_float))  # Sawtooth closed form
+            # Phase shift (by normalization to range [0,1] and modulo operation)
+            sawtooth_wave = (((sawtooth_wave + 1) / 2) + phase / TWO_PI) % 1
+            sawtooth_wave = amp * (sawtooth_wave * 2 - 1)  # re-normalization to range [-amp, amp]
+
+            if isinstance(waveform, str):
+                if waveform == 'sine':
+                    oscillator = sine_wave
+                elif waveform == 'square':
+                    oscillator = square_wave
+                elif waveform == 'triangle':
+                    oscillator = triangle_wave
+                elif waveform == 'sawtooth':
+                    oscillator = sawtooth_wave
+
+            else:
+                waveform_probabilities = waveform[i]
+                oscillator = waveform_probabilities[0] * sine_wave \
+                             + waveform_probabilities[1] * square_wave \
+                             + waveform_probabilities[2] * triangle_wave \
+                             + waveform_probabilities[3] * sawtooth_wave
 
             self.signal[i] = oscillator
 
@@ -142,32 +150,49 @@ class Signal:
         self.signal_values_sanity_check(amp_c, freq_c, waveform)
         t = self.time_samples
         for i in range(num_sounds):
-            if num_sounds > 1:
-                waveform_str = waveform[i]
-                amp_float = amp_c[i]
-                mod_index_float = mod_index[i]
-                freq_float = freq_c[i]
-                input_signal_cur = input_signal[i]
-            else:
-                waveform_str = waveform
+            if num_sounds == 1:
                 amp_float = amp_c
                 mod_index_float = mod_index
                 freq_float = freq_c
                 input_signal_cur = input_signal
+            else:
+                amp_float = amp_c[i]
+                mod_index_float = mod_index[i]
+                freq_float = freq_c[i]
+                input_signal_cur = input_signal[i]
 
-            if waveform_str == 'sine':
-                self.signal[i] = amp_float * torch.sin(TWO_PI * freq_float * t + mod_index_float * input_signal_cur)
-            if waveform_str == 'square':
-                self.signal[i] = amp_float \
-                                 * torch.sign(torch.sin(TWO_PI * freq_float * t + mod_index_float * input_signal_cur))
-            if waveform_str == 'triangle':
-                self.signal[i] = \
-                    (2 * amp_float / PI) * torch.arcsin(
-                        torch.sin((TWO_PI * freq_float * t + mod_index_float * input_signal_cur)))
-            if waveform_str == 'sawtooth':
-                oscillator = 2 * (t * freq_float - torch.floor(0.5 + t * freq_float))
-                oscillator = (((oscillator + 1) / 2) + mod_index_float * input_signal_cur / TWO_PI) % 1
-                self.signal[i] = amp_float * (oscillator * 2 - 1)
+            fm_sine_wave = amp_float * torch.sin(TWO_PI * freq_float * t + mod_index_float * input_signal_cur)
+            fm_square_wave = amp_float \
+                          * torch.sign(torch.sin(TWO_PI * freq_float * t + mod_index_float * input_signal_cur))
+            fm_triangle_wave = (2 * amp_float / PI) \
+                            * torch.arcsin(torch.sin((TWO_PI * freq_float * t + mod_index_float * input_signal_cur)))
+            fm_sawtooth_wave = 2 * (t * freq_float - torch.floor(0.5 + t * freq_float))
+            fm_sawtooth_wave = (((fm_sawtooth_wave + 1) / 2) + mod_index_float * input_signal_cur / TWO_PI) % 1
+            fm_sawtooth_wave = amp_float * (fm_sawtooth_wave * 2 - 1)
+
+            if isinstance(waveform, str):
+                if waveform == 'sine':
+                    oscillator = fm_sine_wave
+                elif waveform == 'square':
+                    oscillator = fm_square_wave
+                elif waveform == 'triangle':
+                    oscillator = fm_triangle_wave
+                elif waveform == 'sawtooth':
+                    oscillator = fm_sawtooth_wave
+
+            else:
+                if num_sounds == 1:
+                    waveform_probabilities = waveform
+                else:
+                    waveform_probabilities = waveform[i]
+
+                oscillator = waveform_probabilities[0] * fm_sine_wave \
+                             + waveform_probabilities[1] * fm_square_wave \
+                             + waveform_probabilities[2] * fm_triangle_wave \
+                             + waveform_probabilities[3] * fm_sawtooth_wave
+
+            self.signal[i] = oscillator
+
 
     def am_modulation(self, amp_c, freq_c, amp_m, freq_m, final_max_amp, waveform):
         """AM modulation
@@ -287,9 +312,9 @@ class Signal:
 
             if num_sounds == 1:
                 attack = torch.linspace(0, 1, attack_num_samples)
-                decay = torch.linspace(1, sustain_level, decay_num_samples)
-                sustain = torch.full((sustain_num_samples,), sustain_level)
-                release = torch.linspace(sustain_level, 0, release_num_samples)
+                decay = torch.linspace(1, sustain_level.item(), decay_num_samples)
+                sustain = torch.full((sustain_num_samples,), sustain_level.item())
+                release = torch.linspace(sustain_level.item(), 0, release_num_samples)
             else:
                 attack = torch.linspace(0, 1, attack_num_samples[i])
                 decay = torch.linspace(1, sustain_level[i], decay_num_samples[i])
@@ -316,17 +341,19 @@ class Signal:
                 plt.show()
 
     def low_pass(self, cutoff_freq, q=0.707, index=0):
-        waveform = torch.clone(self.signal[index])
-        self.signal[index] = taF.lowpass_biquad(waveform, self.sample_rate, cutoff_freq, q)
+        waveform = self.signal[index]
+        filtered_waveform = taF.lowpass_biquad(waveform, self.sample_rate, cutoff_freq, q)
+        return filtered_waveform
 
     def high_pass(self, cutoff_freq, q=0.707, index=0):
-        waveform = torch.clone(self.signal[index])
-        self.signal[index] = taF.highpass_biquad(waveform, self.sample_rate, cutoff_freq, q)
+        waveform = self.signal[index]
+        filtered_waveform = taF.highpass_biquad(waveform, self.sample_rate, cutoff_freq, q)
+        return filtered_waveform
 
     def band_pass(self, central_freq, q=0.707, const_skirt_gain=False, index=0):
-        waveform = torch.clone(self.signal[index])
-        self.signal[index] = \
-            taF.bandpass_biquad(waveform, self.sample_rate, central_freq, q, const_skirt_gain)
+        waveform = self.signal[index]
+        filtered_waveform = taF.bandpass_biquad(waveform, self.sample_rate, central_freq, q, const_skirt_gain)
+        return filtered_waveform
 
     @staticmethod
     # todo: remove all except list instances
@@ -344,12 +371,9 @@ class Signal:
         elif isinstance(amp, list):
             if any(element < 0 or element > 1 for element in amp):
                 raise ValueError("Provided amplitude is not in range [0, 1]")
-        # if len(waveform) == 1:
-        #     if waveform[0][0] not in ['sine', 'square', 'triangle', 'sawtooth']:
-        #         raise ValueError("Unknown waveform provided")
-        # else:
-        if not any(x in waveform for x in ['sine', 'square', 'triangle', 'sawtooth']):
-            raise ValueError("Unknown waveform provided")
+        if isinstance(waveform, str):
+            if not any(x in waveform for x in ['sine', 'square', 'triangle', 'sawtooth']):
+                raise ValueError("Unknown waveform provided")
 
 
 """

src/train.py
@@ -24,26 +24,38 @@ def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
     criterion_spectrogram = nn.MSELoss()
 
     for signal_log_mel_spec, target_params_dic in data_loader:
+
+        if torch.any(torch.isnan(signal_log_mel_spec)):
+            a = 0
+
         start = time.time()
 
         if DEBUG_MODE:
             helper.plot_spectrogram(signal_log_mel_spec[0][0].cpu(), title="MelSpectrogram (dB)", ylabel='mel freq')
 
         optimizer_arg.zero_grad()
+        model.zero_grad()
         # -------------------------------------
         # -----------Run Model-----------------
         # -------------------------------------
+        # signal_log_mel_spec.requires_grad = True
         output_dic = model(signal_log_mel_spec)
 
-        helper.map_classification_params_from_ints(predicted_dic)
-        normalizer.denormalize(predicted_dic)
-        helper.clamp_regression_params(predicted_dic)
+        for key, value in output_dic.items():
+            if torch.any(torch.isnan(value)):
+                a = 0
+
+        # helper.map_classification_params_from_ints(predicted_dic)
+        normalizer.denormalize(output_dic)
+        helper.clamp_regression_params(output_dic)
 
         # -------------------------------------
         # -----------Run Synth-----------------
         # -------------------------------------
-        overall_synth_obj = SynthBasicFlow(parameters_dict=predicted_dic, num_sounds=len(signal_log_mel_spec))
+        overall_synth_obj = SynthBasicFlow(parameters_dict=output_dic, num_sounds=len(signal_log_mel_spec))
 
+        if torch.any(torch.isnan(signal_log_mel_spec)):
+            a = 0
         overall_synth_obj.signal = helper.move_to(overall_synth_obj.signal, device_arg)
 
         predicted_log_mel_spec_sound_signal = helper.log_mel_spec_transform(overall_synth_obj.signal)
@@ -58,31 +70,31 @@ def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
         # -----------------------------------------------
         # -----------Compute sound individually----------
         # -----------------------------------------------
-        loss_spectrogram_total = 0
-        current_predicted_dic = {}
-        for i in range(len(signal_log_mel_spec)):
-            for key, value in predicted_dic.items():
-                if torch.is_tensor(predicted_dic[key][i]):
-                    current_predicted_dic[key] = predicted_dic[key][i].item()
-                else:
-                    current_predicted_dic[key] = predicted_dic[key][i]
 
-            # Generate sound from predicted parameters
-            synth_obj = SynthBasicFlow(parameters_dict=current_predicted_dic)
+        if False:
+            loss_spectrogram_total = 0
+            current_predicted_dic = {}
+            for i in range(len(signal_log_mel_spec)):
+                for key, value in output_dic.items():
+                    current_predicted_dic[key] = output_dic[key][i]
 
-            synth_obj.signal = helper.move_to(synth_obj.signal, device_arg)
-            predicted_log_mel_spec_sound_signal = helper.log_mel_spec_transform(synth_obj.signal)
 
-            predicted_log_mel_spec_sound_signal = helper.move_to(predicted_log_mel_spec_sound_signal, device_arg)
+                # Generate sound from predicted parameters
+                synth_obj = SynthBasicFlow(parameters_dict=current_predicted_dic)
 
-            signal_log_mel_spec = torch.squeeze(signal_log_mel_spec)
-            predicted_log_mel_spec_sound_signal = torch.squeeze(predicted_log_mel_spec_sound_signal)
-            target_log_mel_spec_sound_signal = signal_log_mel_spec[i]
-            current_loss_spectrogram = criterion_spectrogram(predicted_log_mel_spec_sound_signal,
-                                                             target_log_mel_spec_sound_signal)
-            loss_spectrogram_total = loss_spectrogram_total + current_loss_spectrogram
+                synth_obj.signal = helper.move_to(synth_obj.signal, device_arg)
+                predicted_log_mel_spec_sound_signal = helper.log_mel_spec_transform(synth_obj.signal)
 
-        loss_spectrogram_total = loss_spectrogram_total / len(signal_log_mel_spec)
+                predicted_log_mel_spec_sound_signal = helper.move_to(predicted_log_mel_spec_sound_signal, device_arg)
+
+                signal_log_mel_spec = torch.squeeze(signal_log_mel_spec)
+                predicted_log_mel_spec_sound_signal = torch.squeeze(predicted_log_mel_spec_sound_signal)
+                target_log_mel_spec_sound_signal = signal_log_mel_spec[i]
+                current_loss_spectrogram = criterion_spectrogram(predicted_log_mel_spec_sound_signal,
+                                                                 target_log_mel_spec_sound_signal)
+                loss_spectrogram_total = loss_spectrogram_total + current_loss_spectrogram
+
+            loss_spectrogram_total = loss_spectrogram_total / len(signal_log_mel_spec)
 
         # -----------------------------------------------
         #          -----------End Part----------
@@ -137,8 +149,8 @@ def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
                 + SPECTROGRAM_LOSS_FACTOR * loss_spectrogram_total
 
         if LOSS_MODE == 'SPECTROGRAM_ONLY':
-            loss = loss_spectrogram_total
-            loss.requires_grad = True
+            loss = loss_spectrogram_total2
+            # loss.requires_grad = True
 
         # backpropogate error and update wights
         loss.backward()
@@ -185,6 +197,7 @@ def train_single_epoch(model, data_loader, optimizer_arg, device_arg):
 
 
 def train(model, data_loader, optimiser_arg, device_arg, epochs):
+    model.train()
     for i in range(epochs):
         print(f"Epoch {i + 1}")
         train_single_epoch(model, data_loader, optimiser_arg, device_arg)
@@ -206,11 +219,11 @@ if __name__ == "__main__":
     synth_net = SynthNetwork().to(device)
 
     # initialize optimizer
-    optimiser = torch.optim.Adam(synth_net.parameters(), lr=LEARNING_RATE)
+    optimizer = torch.optim.Adam(synth_net.parameters(), lr=LEARNING_RATE, weight_decay=1e-4)
 
     # train model
     print("Training model with LOSS_MODE: ", LOSS_MODE)
-    train(synth_net, train_dataloader, optimiser, device, EPOCHS)
+    train(synth_net, train_dataloader, optimizer, device, EPOCHS)
 
     # save model
     torch.save(synth_net.state_dict(), "../trained_models/synth_net.pth")